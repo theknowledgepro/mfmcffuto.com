d96330a6c8f628a1028d06442eca45f292797ca8
theknowledgepro <ajibopromise10@gmail.com>
2023-07-11 21:06:48 +0100

    cppl

--- components/no_data_found.jsx
+++ components/no_data_found.jsx
@@ -6,10 +6,10 @@ import Image from 'next/image';
 
 const NoDataFound = ({ sorryText, hideLaterText, renderAction }) => {
 	return (
-		<div className='py-5 d-flex flex-column align-items-center justify-content-center w-100 h-100'>
-			<Image src={NoData} className='mt-4 w-100 h-100' style={{ maxWidth: '700px', maxHeight: '450px' }} />
-			<div className='fw-bold fs-5 text-center mt-5 mb-4 w-100 color-primary'>{sorryText}</div>
-			{!hideLaterText && <div className='text-center text-secondary fs-7'>Please check in again at a later time...</div>}
+		<div className='py-5 flex flex-col items-center justify-center w-full h-full'>
+			<Image src={NoData} className='mt-4 w-full h-full' style={{ maxWidth: '700px', maxHeight: '450px' }} />
+			<div className='font-medium-custom fs-5 text-center mt-5 mb-4 w-full color-primary'>{sorryText}</div>
+			{!hideLaterText && <div className='text-center text-gray-500 text-[16px]'>Please check in again at a later time...</div>}
 			{renderAction}
 		</div>
 	);
