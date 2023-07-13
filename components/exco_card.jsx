a9d37fba9e93967780d59b869c6a1f9fdb83c9a2
theknowledgepro <ajibopromise10@gmail.com>
2023-07-13 12:20:28 +0100

    cool

--- components/exco_card.jsx
+++ components/exco_card.jsx
@@ -5,9 +5,17 @@ import { CLOUD_ASSET_BASEURL, S3FOLDERS } from '@/config';
 import ImageTag from './image_tag';
 
 const ExcoCard = ({ exco, wrapperClassName, isActiveIndex }) => {
+	const [isActive, setIsActive] = React.useState(isActiveIndex);
+	React.useEffect(() => {
+		setIsActive(isActiveIndex);
+	}, [isActiveIndex]);
+
+	const handleToggleIsActiveIndex = () => setIsActive(!isActive);
+
 	return (
 		<div className='xss:px-2 xss:py-4 sm:p-4 w-full max-w-[400px] max-h-[400px] h-[350px]'>
 			<div
+				onClick={handleToggleIsActiveIndex}
 				className={`flex flex-col items-center justify-center overflow-hidden shadow-[0_0_3px_rgba(166,53,144,0.8)] border w-full h-full relative rounded-[5px] ${wrapperClassName}`}>
 				<ImageTag
 					style={{}}
@@ -17,7 +25,7 @@ const ExcoCard = ({ exco, wrapperClassName, isActiveIndex }) => {
 				/>
 				<div
 					className={`absolute bottom-[-35%] w-[90%] h-[50%] rounded-[5px] ${
-						isActiveIndex && 'translate-y-[-45%]'
+						isActive && 'translate-y-[-45%]'
 					} hover:translate-y-[-45%] transition-all ease-out duration-300 py-3 px-2 border border-zinc-300 bg-white flex flex-col shadow-[0_0_3px_rgba(166,53,144,0.8)]`}>
 					<div className='w-full text-center font-medium-custom line-height-1b mb-1'>{`${exco?.lastname ? exco?.lastname : ''} ${
 						exco?.firstname ? exco?.firstname : ''
